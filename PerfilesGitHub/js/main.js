032e408d0a419937b0d068847f53ffbcee2b5cbe
MissValen <valen2409s@gmail.com>
2022-05-19 19:01:43 -0500

    Hicimos un widget

--- PerfilesGitHub/js/main.js
+++ PerfilesGitHub/js/main.js
@@ -1,16 +1,21 @@
 const API = "https://api.github.com/users/";
 
-async function Buscar(){
-    const response =await fetch(API + 'MissValen')
-    //ahoar quiero traer la info en formato json,
-    const data = await response.json()
-    console.log(data)
-}
-
 const app = Vue.createApp({
     data() {
         return {
-          message: 'Hello SENA!'
+          message: 'Hello SENA!',
+          busqueda: null,
+          result: null
         }
-      }
+    },
+// La palabra funtion ya no es necesario ya que se una un metodo
+    methods: {
+        async Buscar(){
+          const response =await fetch(API + this.busqueda)
+          //ahoar quiero traer la info en formato json,
+          const data = await response.json()
+          console.log(data)
+          this.result = true
+        }
+    }
 })//.mount('#app') montamos esta informacion el html del div app
